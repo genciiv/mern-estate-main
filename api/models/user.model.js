cb96579e604365f55558e41ccaea962ef0b87558
G-code <30412971+genciiv@users.noreply.github.com>
2023-11-20 11:31:41 +0100

    create and add OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar: {
+      type: String,
+      default:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw3ZcnGkt9UeBwGfneourr7R&ust=1700561855138000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCLCPrtys0oIDFQAAAAAdAAAAABAE",
+    },
   },
   { timestamps: true }
 );
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2trt5evttkhydSerm04vlVwFLSFJ-tec",
+  authDomain: "mern-estate-1cb51.firebaseapp.com",
+  projectId: "mern-estate-1cb51",
+  storageBucket: "mern-estate-1cb51.appspot.com",
+  messagingSenderId: "346368051340",
+  appId: "1:346368051340:web:96fab7f97d2758d86e93a9",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
